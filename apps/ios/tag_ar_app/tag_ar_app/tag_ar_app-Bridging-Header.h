24d3379f660b1822cf15e0052188dfe36d38e336
ysjeung <yunsukjeung@snu.ac.kr>
2026-06-20 14:20:06 +0900

    feat: ios project

---
+++ apps/ios/tag_ar_app/tag_ar_app/tag_ar_app-Bridging-Header.h
@@ -0,0 +1,12 @@
+//
+//  tag_ar_app-Bridging-Header.h
+//  tag_ar_app
+//
+//  Created by Yunsuk Jeung on 20/6/2026.
+//
+
+//
+//  Use this file to import your target's public headers that you would like to expose to Swift.
+//
+
+#import "ShaderTypes.h"
